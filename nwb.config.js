e6a82e54a9171e9e0ca880f807ec3cd4ab3174df
Patrick Smith <patrick@burntcaramel.com>
2017-11-14 21:41:27 +1100

    Fix umd build
    Remove preact

--- nwb.config.js
+++ nwb.config.js
@@ -4,7 +4,9 @@ module.exports = {
     esModules: true,
     umd: {
       global: 'PreactOrganism',
-      externals: {}
+      externals: {
+        'preact': 'preact'
+      }
     }
   },
   babel: {
